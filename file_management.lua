f6f148eb5defe4586cd7969da0799739d9be2168
Silvan Schmidt <sil.schmidt@proton.me>
2023-06-17 16:39:31 +0200

    add save file

--- file_management.lua
+++ file_management.lua
@@ -5,6 +5,7 @@ return {
     mappings = {
         n = {
             ["<leader>n"] = utils.wrap_func("new_file"),
+            ["<leader>w"] = utils.wrap_func("save_file"),
         },
     },
     funcs = {
@@ -12,7 +13,20 @@ return {
             func = function()
                 vim.cmd("enew")
             end,
-            desc = "New file",
+            desc = "Create a new file",
         },
+        save_file = {
+            func = function()
+                local file_name = vim.fn.bufname()
+                if file_name == "" then
+                    file_name = vim.fn.input("Filename (esc or leave empty to cancel): ", "", "file")
+                    if file_name == nil or file_name == "" then
+                        return
+                    end
+                end
+                vim.cmd("write " .. file_name)
+            end,
+            desc = "Save the current file", 
+        }
     },
 }
