b9ace52091dd959117c788f28f09386c0a8332cd
Silvan Schmidt <sil.schmidt@proton.me>
2023-06-17 17:13:06 +0200

    add func mapping

--- init.lua
+++ init.lua
@@ -1,9 +1,12 @@
 return {
+    mappings = {
+        n = {
+            ["<leader>p"] = "<cmd>Func<cr>",
+        },
+    },
     deps = {
-	"basic.opts",
-
+    	"basic.opts",
         "basic.file_management",
-
         "basic.parentheses",
     }
 }
